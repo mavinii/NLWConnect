c690b4cb668384b06b1b69665e78fda0f8a539d6
Marcos Oliveira <2023323@student.cct.ie>
2025-02-26 16:19:35 +0000

    feat: :art: First Page
    Landing page and README updated.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# NLWConnect
+<p>
+    <h1 align="center">🎉 NLWConnect: CodeCraftSummit 2025</h1>
+</p>
+
+## 📝 About:
+NLWConnect: CodeCraftSummit 2025 is a full-stack Invite Event Indication project built using `React`, `Next.js`, `Fastify`, `RESTful API`, `SQL`, `Swagger`, and data validation.
+
+This project was developed during NLWConnect, a free event held from February 17th to 20th, 2025, organized by Rocketseat. The event brought together thousands of developers worldwide to learn, share knowledge, and network with like-minded professionals.
+
+<p align="center">
+  <img alt="NLWConnect" src="./preview/preview_lp.jpg" >
+</p>
+
+## © Copyright:
+Please note, this is a simple project as a IT student, however, it may contain some part of the code that may be Copyright. Please also note that this project is `non-profit` and not intended to be monetized.
+
+<strong>Thanks to [RocketSeat](https://www.rocketseat.com.br).</strong>
+
+---
+
+<strong>Built with 💙 by [@Marcos Oliveira](https://www.linkedin.com/in/pgmarcosoliveira/)</strong>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,69 @@
+import Image from 'next/image';
+
+import logo from '../assets/logo.svg';
+import { ArrowRight, Mail, Radio, User } from 'lucide-react';
+import { InputField, InputIcon, InputRoot } from '@/components/input';
+import { Button } from '@/components/button';
+
 export default function Home() {
   return (
-    <main></main>
+    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+      <div className="min-h-dvh flex flex-col justify-center gap-16">
+        
+        {/* DIV dev stage and CodeCraft Summit 2025 */}
+        <div className="flex flex-col gap-8 items-center md:items-start">
+          <Image src={logo} alt='DevStage Logo' width={108.5} height={30} />
+
+          <h1 className='text-6xl text-center leading-none font-heading font-medium flex flex-col md:text-7xl md:text-left'>
+            <span className='text-blue'>CodeCraft</span>
+            Summit 2025
+          </h1>
+        </div>
+
+        {/* DIV About the event and Register */}
+        <div className='flex gap-5 items-stretch flex-col md:flex-row'>
+          <div className='flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6'>
+            <div className='flex items-center justify-between'>
+              <h2 className='font-heading font-bold text-gray-100 text-xl'>About this Event</h2>
+              <span className='text-danger font-semibold text-xs flex gap-2 items-center'>
+                <Radio className='size-5' />
+                LIVE
+              </span>
+            </div>
+            
+            <p className='text-gray-300 leading-relaxed text-sm md:text-base'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, ad quam minima voluptate voluptatum sit sapiente officiis libero recusandae asperiores est ab similique doloribus deleniti perferendis quasi voluptas? Alias, qui.
+              <br /><br />
+              On 15th, 16th and 17th of October from 18h to 21h | <span className='font-bold text-gray-200'>Online</span> and <span className='font-bold text-gray-200'>Free</span>.
+            </p>
+          </div>
+
+          <form className='bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[441px]'>
+            <h2 className='font-heading font-bold text-gray-100 text-xl'>Register</h2>
+
+            <div className='space-y-3'>
+              <InputRoot>
+                <InputIcon>
+                  <User className='size-5' />
+                </InputIcon>
+                <InputField type='text' placeholder='Full Name' />
+              </InputRoot>
+
+              <InputRoot>
+                <InputIcon>
+                  <Mail className='size-5' />
+                </InputIcon>
+                <InputField type='text' placeholder='john@example.com' />
+              </InputRoot>
+            </div>
+
+            <Button type='submit'>
+              Register
+              <ArrowRight className='size-5' />
+            </Button>
+          </form>
+        </div>
+      </div>
+    </main>
   );
 }
